ce5fcae8b3db3c03fa3654a09564925f54c2d8ad
localist-richardparsons <localist@richardparsons.de>
2025-09-29 14:55:40 +0200

    Remove unused import

--- pages/epk.tsx
+++ pages/epk.tsx
@@ -1,7 +1,6 @@
 import { FaBandcamp, FaInstagram, FaSpotify } from "react-icons/fa";
 
 import Head from "next/head";
-import Image from "next/image";
 import type { NextPage } from "next";
 import styles from "../styles/epk.module.css";
 
